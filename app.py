eff22df4909e8b49ab55a8803c9de307f55cd2ed
gt-03 <103998515+gt-03@users.noreply.github.com>
2024-07-18 01:34:39 +0800

    Improved algroithm and text formatted

--- app.py
+++ app.py
@@ -1,26 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, Response
 import os
+from datetime import datetime
+from motion_detection import generate_frames
 import subprocess
 
 app = Flask(__name__)
-
-# def get_video_metadata(video_path):
-#     try:
-#         result = subprocess.run(
-#             ["ffprobe", "-v", "error", "-of", "compact", video_path, "-show_entries", "format_tags=comment"],
-#             stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
-#         )
-#         # metadata = result.stdout.strip()
-#         print(result.stdout.strip())
-#         if metadata.startswith("format_tags_comment="):
-#             return metadata[len("format_tags_comment="):].strip('"')
-#         return "No comment"
-#     except Exception as e:
-#         print(f"Error getting metadata for {video_path}: {e}")
-#         return "Error retrieving comment"
-
-
-# print(get_video_metadata("static/test.vid.mp4"))
 
 @app.route('/')
 def index():
@@ -28,5 +12,70 @@ def index():
 	clips = [f for f in os.listdir(static_folder) if f.endswith('.mp4')]
 	return render_template('index.html', clips = clips)
 
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/delete/<clip_name>')
+def delete_clip(clip_name):
+    clip_path = os.path.join(app.root_path, 'static', clip_name)
+    if os.path.exists(clip_path):
+        os.remove(clip_path)
+    return redirect(url_for('clips'))
+
+@app.route('/clips')
+def clips():
+    static_folder = os.path.join(app.root_path, 'static')
+    clips = [f for f in os.listdir(static_folder) if f.endswith('.mp4')]
+
+    # Get the full path for each clip
+    clips_full_path = [os.path.join(static_folder, f) for f in clips]
+    
+    # Sort the clips by their creation time in descending order
+    clips_sorted = sorted(clips_full_path, key=os.path.getctime, reverse=True)
+
+    # Extract the filenames from the sorted full paths and format the creation time
+    formatted_clips = []
+    for clip_path in clips_sorted:
+        clip_name = os.path.basename(clip_path)
+        timestamp = get_timestamp_from_filename(clip_path)
+        comment = get_video_metadata(clip_path)
+        formatted_clips.append((clip_name, timestamp, comment))
+
+    return render_template('clips.html', clips=formatted_clips)
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+def get_timestamp_from_filename(filepath):
+    # Extract the filename from the path
+    filename = os.path.basename(filepath)
+
+    # Extract the timestamp string from the filename by removing the file extension
+    timestamp_str = filename.split('.')[0]  
+
+    # Parse the timestamp string into a datetime object
+    timestamp = datetime.strptime(timestamp_str, "%Y-%m-%d_%H-%M-%S")
+
+    # Format the datetime object into the string new format
+    formatted_timestamp = timestamp.strftime("%I:%M:%S %p %d %B %Y")
+    
+    return formatted_timestamp
+
+def get_video_metadata(file_path):
+    cmd = [
+        "ffmpeg",
+        "-i", file_path,
+        "-f", "ffmetadata",
+        "-"
+    ]
+    result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    
+    metadata = {}
+    for line in result.stdout.splitlines():
+        if "=" in line:
+            key, value = line.split("=", 1)
+            metadata[key.strip()] = value.strip()
+    
+    return metadata.get("comment", "")
+
--- email_alert.py
+++ email_alert.py
@@ -10,6 +10,9 @@ def format_object_detected(labels):
     Counts the frequency of each label and formats it as a comma-separated string.
     Example: "2 persons, 1 toilet, 1 vase"
     """
+    if len(labels) == 0:
+        return "Unidentifiable object"
+    
     object_counter = Counter(labels)
     objects_detected = ", ".join([f"{count} {label}" for label, count in object_counter.items()])
     return objects_detected
@@ -24,6 +27,10 @@ def initialize_email_server():
 
     return server, email_address
 
+    # This is an automated alert to inform you that motion has been detected.
+
+    # You will receive another email with the further details of the objects detected once the clip is processed. 
+
 
 def send_first_email_alert(server, email_address):
     """Sends an email notification indicating that motion has been detected."""
@@ -34,19 +41,20 @@ def send_first_email_alert(server, email_address):
     message["Subject"] = "Motion Detected" 
     
     message_body = """
-    Hello
+    Hello Sir/Mdm,
 
-    This is an automated alert to inform you that motion has been detected.
+    1 person has been detected in the video feed.
 
-    You will receive another email with the further details of the objects detected once the clip is processed. 
+    Please visit the dashboard to view the recorded clip.
 
-    Thank you
+    Thank you,
+    MySentry
     """
 
     message.attach(MIMEText(message_body, "plain"))
     server.sendmail(email_address, email_address, message.as_string())
 
-def send_second_email_alert(server, email_address, labels):
+def send_second_email_alert(server, email_address, labels, current_time):
     """Sends an email notification indicating the number of objects detected; defaults to 1 object."""
     message = MIMEMultipart()
     
@@ -54,24 +62,26 @@ def send_second_email_alert(server, email_address, labels):
     message["To"] = email_address
 
     # Get the current date and time
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # current_time = datetime.now().strftime("%I:%M:%S %p %d %B %Y")
     message["Subject"] = f"Motion Detected - {current_time}" 
     
     # Get the formatted object details
     object_details = format_object_detected(labels)
 
     message_body = f"""
-    Hello
+    Hello,
 
     This is an automated alert to inform you that motion has been detected.
 
-    Object(s) detected at {current_time}:
+    Objects detected at {current_time}:
     {object_details}
 
-    Please visit the dashboard to view the playback clip.
+    Please visit the dashboard to view the recorded clip.
 
-    Thank you
+    Thank you,
+    MySentry
     """
 
     message.attach(MIMEText(message_body, "plain"))
     server.sendmail(email_address, email_address, message.as_string())
+    print("Sent email alert")
